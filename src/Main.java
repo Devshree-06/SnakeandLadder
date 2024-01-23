1e71c10bc6e3c2284bd874d49d5df6af89c8f6f3
Devshree Moghe <devshree0602@gmail.com>
2024-01-23 21:18:01 +0530

    Inital

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int dice=(int)Math.random();
+        int pos=0;
+        int option=Math.random();
+        String new_option;
+
+       if(new_option=="No play"){
+           pos=curr;
+       }
+       else if(new_option=="Ladder"){
+           pos++;
+       }
+       else{
+           pos--;
+       }
+
+
+    }
+}
---
+++ src/Players.java
@@ -0,0 +1,10 @@
+public class Players {
+    String name;
+    int currentpos;
+
+    Players(String n){
+        this.name=n;
+        this.currentpos=0;
+    }
+
+}
